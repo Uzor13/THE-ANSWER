eaa3ea3a62888ccb266f4c23cdc22d6d0ba92671
ed-knowah <edetnoah@gmail.com>
2021-08-23 21:51:00 +0100

    committed

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const isAuth = require("../controllers/auth").isAuth;
-
 const adminController = require("../controllers/adminController");
-
-//team stats
-//player stats
 
 router.get("/allPlayers", adminController.getAllPlayers);
 router.get("/fixtures", adminController.getFixtures);
 router.get("/teamplayers/:teamName", adminController.getTeamPlayers);
 router.get("/logout", adminController.logoutAdmin);
 
-router.post("/login", adminController.loginAmin);
 router.post("/register", adminController.createAdmin);
+router.post("/login", adminController.loginAmin);
 router.post("/addFixtures", isAuth, adminController.addFixtures);
 router.post("/addPlayer", isAuth, adminController.addPlayer);
 router.post("/addTeam", isAuth, adminController.addTeam);
